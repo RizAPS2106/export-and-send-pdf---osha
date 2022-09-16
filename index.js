6d23362343e71a0497a5ac06e1552ca53ede9b9f
rizky <rizkyaditya2106@gmail.com>
2022-09-16 16:20:09 +0700

    error fix try

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const bodyParser = require('body-parser')
 const pdf = require('html-pdf')
 const http = require('http')
 const nodemailer = require('nodemailer')
-
 const pdfTemplate = require('./documents')
 const pdfOptions = {
     "format": "A4",
@@ -17,7 +16,64 @@ const pdfOptions = {
     }
 }
 
-const app = require("./src/app")
+const app = express()
+
+const { port, allowed_domains } = config
+
+app.use(cors({origin : allowed_domains}))
+app.use(helmet())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+// POST PDF generation and fetching of the data
+app.post('/create-pdf', (req,res,next) => {
+    console.log(req.body)
+    pdf.create(pdfTemplate(req.body), pdfOptions).toFile('result.pdf', (err)=>{
+        if(err) {
+            res.send(Promise.reject().catch(next))
+        } else {
+            res.send(Promise.resolve())
+        }
+    }) 
+})
+
+// GET Send the generated PDF to the client
+app.get('/fetch-pdf', (req, res) => {
+    res.sendFile(`${__dirname}/result.pdf`)
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    })
+    
+    let mailOptions = {
+        from: 'rizkyaditya2106@gmail.com',
+        to: 'rizkyaditya.osha@gmail.com',
+        cc: '',
+        bcc: '',
+        subject: 'Applicant CV',
+        text: 'Hallo, here is new applicant',
+        attachments: [
+            { filename: 'result.pdf', path: './result.pdf' }
+        ]
+    }
+
+    transporter.sendMail(mailOptions, function(err, info) {
+        if(err) {
+            res.send(console.log('Error'))
+        } else {
+            res.send(console.log('Message Sent'))
+        }
+    })
+})
+
+// Page
+app.get('/', (req, res) => {
+    return res.send(`This is OSHA Technology page server, ${allowed_domains}`)
+})
 
 const server = http.createServer(app)
 
